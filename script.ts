c7ae98f7c8e27cb39181f516f8413f8dab610605
Marek Tacina <marek.tacina@gmail.com>
2023-03-11 15:06:02 +0100

    Přidány karty pro počasí Karpentná a Radomsko

--- script.ts
+++ script.ts
@@ -49,8 +49,52 @@ function vypisKurzyNBP() {
     }
 }
 
+
+function vypisPocasi(lat: number, lon: number, lokalita: string) {
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?altitude=400&lat=${lat}&lon=${lon}`;
+    stahniJSON(url, (predpoved) => {
+
+        for (let i = 1; i < 13; i++) {
+            let cas = (predpoved.properties.timeseries[i].time).substr(11,2);
+            let teplota = Math.round(Number(predpoved.properties.timeseries[i - 1].data.instant.details.air_temperature));
+
+            let srazky = predpoved.properties.timeseries[i - 1].data.next_1_hours.details.precipitation_amount;
+            srazky = (srazky.toString().includes('.') ? srazky : srazky + '.0');
+            srazky = srazky == "0.0" ? "" : srazky;
+
+            let vitr = Math.round(Number(predpoved.properties.timeseries[i - 1].data.instant.details.wind_speed));
+
+            let containerPocasi = document.querySelector(`.${lokalita}`);
+            let radek = document.createElement("div");
+            radek.className = "row";
+            let sloupecHodiny = document.createElement("div");
+            sloupecHodiny.className = "col";
+            sloupecHodiny.innerHTML = `${cas}`;
+            let sloupecTeplota = document.createElement("div");
+            sloupecTeplota.innerHTML = `${teplota}`;
+            sloupecTeplota.className = teplota >= 0 ? "col text-danger" : "col text-primary";
+            let sloupecSrazky = document.createElement("div");
+            sloupecSrazky.innerHTML = `${srazky}`;
+            sloupecSrazky.className = "col";
+            let sloupecVitr = document.createElement("div");
+            sloupecVitr.innerHTML = `${vitr}`;
+            sloupecVitr.className = "col";
+
+            radek.appendChild(sloupecHodiny);
+            radek.appendChild(sloupecTeplota);
+            radek.appendChild(sloupecSrazky);
+            radek.appendChild(sloupecVitr);
+            containerPocasi.appendChild(radek);
+        }
+    });
+}
+
+
 vypisJmena();
 vypisKurzyNBP();
+vypisPocasi(49.6336822, 18.6824547, "pocasi-karpentna");
+vypisPocasi(51.0717922, 19.4338617, "pocasi-radomsko");
+
 
 
 
